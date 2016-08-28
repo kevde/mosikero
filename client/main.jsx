c73fd7674be55c473d9b2bb0e449cb3bfb1cd7a7
kevde <aronkev@gmail.com>
2016-08-29 01:24:05 +0800

    Improve with MDG geolocation

--- client/main.jsx
+++ client/main.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import { render } from 'react-dom';
+import { GoogleMaps } from 'google-maps';
 
 import '../imports/startup/accounts-config.js';
 import App from '../imports/ui/App.jsx';
 
+let MAP_ZOOM = 15;
+
 Meteor.startup(() => {
+  GoogleMaps.load();
   render(<App />, document.getElementById('render-target'));
 });
